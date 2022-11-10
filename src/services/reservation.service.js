c81bff12da9dd05d24348463be2331e7875fe2ec
JuusoMRusanen <juuso.m.rusanen@gmail.com>
2022-11-10 23:38:02 +0200

    created service file

---
+++ src/services/reservation.service.js
@@ -0,0 +1,29 @@
+import http from "../http-common";
+
+class ReservationDataService {
+  getAll() {
+    return http.get("/reservations");
+  }
+
+  get(id) {
+    return http.get(`/reservations/${id}`);
+  }
+
+  create(data) {
+    return http.post("/reservations", data);
+  }
+
+  update(id, data) {
+    return http.put(`/reservations/${id}`, data);
+  }
+
+  delete(id) {
+    return http.delete(`/reservations/${id}`);
+  }
+
+  deleteAll() {
+    return http.delete(`/reservations`);
+  }
+}
+
+export default new ReservationDataService();
