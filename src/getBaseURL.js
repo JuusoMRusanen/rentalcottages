792dfda3d1b66e10e578d9fcafdbefd012335429
JuusoMRusanen <juuso.m.rusanen@gmail.com>
2022-11-11 22:57:29 +0200

    added function to give baseurl

---
+++ src/getBaseURL.js
@@ -0,0 +1,10 @@
+export default function getBaseURL() {
+
+  // DEFINE SERVER URL
+
+  //const baseURL "https://rentalcottages.herokuapp.com/" // PRODUCTION URL
+
+  const baseURL = "http://localhost:5000"; // DEVELOPMENT URL
+
+  return baseURL;
+};
