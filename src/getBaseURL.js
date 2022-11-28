eed708adc8a2de566ab2d6059a99cb8dd521196b
JuusoMRusanen <juuso.m.rusanen@gmail.com>
2022-11-28 13:28:51 +0200

    baseurl change

--- src/getBaseURL.js
+++ src/getBaseURL.js
@@ -3,9 +3,9 @@ export default function getBaseURL() {
   // DEFINE SERVER URL
   //const baseURL = "http://192.168.0.161:8080"; // NETWORK URL 
 
-  const baseURL = "https://rentalcottagesapi.herokuapp.com"; // PRODUCTION URL
+  //const baseURL = "https://rentalcottagesapi.herokuapp.com"; // PRODUCTION URL
 
-  //const baseURL = "http://localhost:8080"; // DEVELOPMENT URL 
+  const baseURL = "http://localhost:8080"; // DEVELOPMENT URL 
 
   return baseURL;
 };
